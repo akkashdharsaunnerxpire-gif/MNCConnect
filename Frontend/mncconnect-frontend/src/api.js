c10bef9c18de0e3b2a139aa9161f7a3e75c8c418
AkkashDharsaun <akkashdharsaun02@gmail.com>
2026-08-06 15:54:13 +0530

    update api.js

--- Frontend/mncconnect-frontend/src/api.js
+++ Frontend/mncconnect-frontend/src/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Pass JWT Token in Headers automatically
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
