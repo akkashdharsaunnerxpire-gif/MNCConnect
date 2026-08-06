6d7041800ba049d1a3973a8e6cd6b2c2deeb4906
AkkashDharsaun <akkashdharsaun02@gmail.com>
2026-08-06 16:24:39 +0530

    update serve.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI); // 👈 Add this
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
